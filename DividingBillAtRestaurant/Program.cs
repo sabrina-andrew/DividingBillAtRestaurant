0d5c5d2a567eb86faf7c150160314288b58c56ef
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 13:52:03 -0500

    Dividing bill 3 ways.

---
+++ DividingBillAtRestaurant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DividingBillAtRestaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //At a restaurant, Mike and his three friends decided to divide the bill evenly. 
+            //If each person paid \$13 then what was the total bill?
+
+            int oneThird = 13;
+            int totalBill;
+
+            totalBill = oneThird * 3;
+
+            Console.WriteLine("The total bill was " + "$" + totalBill + ".");
+        }
+    }
+}
